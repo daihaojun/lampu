5b8766a8242a512bbc10a296a24ac52d3198fa5b
Dennis <dai.haojun@gmail.com>
2014-07-06 11:42:37 +0800

    install required module

--- puppet/manifests/install.pp
+++ puppet/manifests/install.pp
@@ -31,3 +31,13 @@ class { 'runtime_project::hiera_setup':
     require => File["/opt/config/${::settings::environment}/config.json"],
 }
 
+file { "/opt/config/production/git/lampu/puppet/install_modules.sh":
+  mode => 700,
+} -> 
+exec { "install modules":
+    command => "install_modules.sh",
+    path    => "/usr/local/bin/:/bin/:/opt/config/production/git/lampu/puppet/",
+    user    => 'root',
+}
+
+
