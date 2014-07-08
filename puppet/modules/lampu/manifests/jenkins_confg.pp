987418e32205c983449e734ac2366fd3e5b1dfd8
haojun.dai@hp.com <haojun.dai@hp.com>
2014-07-08 14:26:49 +0800

    fix error

--- puppet/modules/lampu/manifests/jenkins_confg.pp
+++ puppet/modules/lampu/manifests/jenkins_confg.pp
@@ -10,7 +10,5 @@ class lampu::jenkins_confg
      ssl_cert_file_contents =>   '',
 		 ssl_key_file_contents =>    '',
 		 ssl_chain_file_contents =>  '',
-		 jenkins_ssh_private_key=>  '',
-		 ca_certs_db =>              '/opt/config/cacerts',
    }  
 }
