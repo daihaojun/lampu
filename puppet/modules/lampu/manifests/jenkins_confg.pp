ef00e2c85a2de6f1d74d7f255bd949a9d5cb07d6
haojun.dai@hp.com <haojun.dai@hp.com>
2014-07-08 14:20:25 +0800

    enable ssl for jenkins

--- puppet/modules/lampu/manifests/jenkins_confg.pp
+++ puppet/modules/lampu/manifests/jenkins_confg.pp
@@ -4,6 +4,13 @@ class lampu::jenkins_confg
        sudo => false,     
    } ->
    class { 'lampu_jenkins::master':
-     
-   }
+     ssl_cert_file =>   "/etc/ssl/certs/%{::fqdn}.pem", 
+     ssl_key_file =>             "/etc/ssl/private/%{::fqdn}.key",
+     ssl_chain_file =>           '/etc/ssl/certs/intermediate.pem',
+     ssl_cert_file_contents =>   '',
+		 ssl_key_file_contents =>    '',
+		 ssl_chain_file_contents =>  '',
+		 jenkins_ssh_private_key=>  '',
+		 ca_certs_db =>              '/opt/config/cacerts',
+   }  
 }
