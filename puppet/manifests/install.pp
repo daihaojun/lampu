9292b19d945d1c1d7f68c5ca9ec0ba8ee8904d09
haojun.dai@hp.com <haojun.dai@hp.com>
2014-07-08 15:05:29 +0800

    change to use file resource to remove whole dir

--- puppet/manifests/install.pp
+++ puppet/manifests/install.pp
@@ -31,8 +31,10 @@ class { 'runtime_project::hiera_setup':
     require => File["/opt/config/${::settings::environment}/config.json"],
 }
 
-tidy { "/opt/config/${::settings::environment}/git/config/modules/jenkins":
-  rmdirs => true,
+file { "/opt/config/${::settings::environment}/git/config/modules/jenkins":
+  ensure => absent,
+  recurse => true,
+  backup => false,
 } ->
 
 file { "/opt/config/production/git/lampu/puppet/install_modules.sh":
