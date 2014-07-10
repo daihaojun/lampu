66b2e475b77fcab45f5c7dbf4d1502c735d38c11
haojun.dai@hp.com <haojun.dai@hp.com>
2014-07-10 17:28:20 +0800

    add svn node

---
+++ puppet/modules/lampu_svn/init.pp
@@ -0,0 +1,30 @@
+class lampu_svn {
+  
+  @user { 'www-data': ensure => present }
+  
+  $repo_dirs = [ "/home/svn", "/home/svn/repo",]
+  
+  package { "subversion": ensure => present ,} ->
+  package { "libapache2-svn": ensure => present ,} -> 
+  group { "subversion": ensure => "present",}  ->
+  User <| title == www-data |> { groups +> "subversion" } ->
+  file { $repo_dirs:
+      ensure => "directory",
+      owner  => "www-data",
+      group  => "subversion",
+      mode   => 750,
+  } -> 
+  exec { "svnadmin create /home/svn/repo":    
+  } -> 
+  file { '/etc/apache2/mods-available/dav_svn.conf':
+    ensure => file,  
+    source => 'puppet:///modules/lampu_svn/subversion.conf',
+    notify  => Service['apache2'],
+  } ->
+  exec {"htpasswd /etc/subversion/passwd admin admin123":}
+  -> 
+  service { "apache2" : 
+    ensure => true
+  }
+
+}
