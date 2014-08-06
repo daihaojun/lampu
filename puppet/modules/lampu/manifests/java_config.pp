2b7c1de332319e101a55ded9e61665937f7bab82
Dennis <dai.haojun@gmail.com>
2014-08-06 20:44:22 +0800

    fix issue

--- puppet/modules/lampu/manifests/java_config.pp
+++ puppet/modules/lampu/manifests/java_config.pp
@@ -1,6 +1,6 @@
 class lampu::java_confg
 {
-  java_ks { 'puppetca:truststore':
+  java_ks { 'hp:/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/security/cacerts':
     ensure       => latest,
     certificate  => '/opt/certs/cert01.crt',
     target       => '/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/security/cacerts',
